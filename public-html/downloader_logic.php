77c7604505d1a1c0eef542bde78428fb0af4df60
Lunae <62279412+lunae-f@users.noreply.github.com>
2025-06-16 12:03:19 +0900

    細部の編集

--- public-html/downloader_logic.php
+++ public-html/downloader_logic.php
@@ -1,10 +1,6 @@
 <?php
 // downloader_logic.php
 
-// 表示用の変数を初期化
-$download_link = "";
-$error_message = "";
-
 // フォームが送信された場合に処理を実行
 if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['video_url'])) {
 
@@ -15,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['video_url'])) {
     $safe_url = escapeshellarg($video_url);
 
     // 実行するコマンドを定義
-    $command = "/usr/local/bin/yt-dlp -f bestvideo[vcodec*=avc1]+bestaudio[acodec*=mp4a] --embed-thumbnail -o 'downloads/%(title)s.%(ext)s' " . $safe_url . " 2>&1";
+    $command = "/usr/local/bin/yt-dlp -f bestvideo[vcodec*=avc1]+bestaudio[acodec*=mp4a] --embed-thumbnail -o 'downloads/%(title)s.%(ext)s' " . $safe_url;
     
     // コマンドを実行し、出力を取得
     $output = shell_exec($command);
