4c5371ceee06154439a82c05871fd887430c4234
Lunae <62279412+lunae-f@users.noreply.github.com>
2025-06-16 12:14:15 +0900

    yt-dlpのファイルパスを修正

--- public-html/downloader_logic.php
+++ public-html/downloader_logic.php
@@ -11,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['video_url'])) {
     $safe_url = escapeshellarg($video_url);
 
     // 実行するコマンドを定義
-    $command = "/usr/local/bin/yt-dlp -f bestvideo[vcodec*=avc1]+bestaudio[acodec*=mp4a] --embed-thumbnail -o 'downloads/%(title)s.%(ext)s' " . $safe_url;
+    $command = "/usr/bin/yt-dlp -f bestvideo[vcodec*=avc1]+bestaudio[acodec*=mp4a] --embed-thumbnail -o 'downloads/%(title)s.%(ext)s' " . $safe_url;
     
     // コマンドを実行し、出力を取得
     $output = shell_exec($command);
