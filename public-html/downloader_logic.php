9d6f96b07941a21984bf44335ca7ea8af010b0a3
Lunae <62279412+lunae-f@users.noreply.github.com>
2025-06-16 11:56:21 +0900

    yt-dlpの出力を修正

--- public-html/downloader_logic.php
+++ public-html/downloader_logic.php
@@ -15,7 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['video_url'])) {
     $safe_url = escapeshellarg($video_url);
 
     // 実行するコマンドを定義
-    $command = "/usr/local/bin/yt-dlp -f bestvideo[vcodec*=avc1]+bestaudio[acodec*=mp4a] --embed-thumbnail --print filename -o 'downloads/%(title)s.%(ext)s' " . $safe_url . " 2>&1";
+    $command = "/usr/local/bin/yt-dlp -f bestvideo[vcodec*=avc1]+bestaudio[acodec*=mp4a] --embed-thumbnail -o 'downloads/%(title)s.%(ext)s' " . $safe_url . " 2>&1";
     
     // コマンドを実行し、出力を取得
     $output = shell_exec($command);
